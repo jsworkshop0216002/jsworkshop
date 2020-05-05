febfa5e984a76a49ea233353d35ba2cb3c858387
shes50103 <shes50103@gmail.com>
2020-05-05 17:50:41 +0800

    fix: fix time column

---
+++ db/migrate/20200505094802_change_start_at_end_at_column_type.rb
@@ -0,0 +1,6 @@
+class ChangeStartAtEndAtColumnType < ActiveRecord::Migration[6.0]
+  def change
+    change_column :products, :start_at, :datetime
+    change_column :products, :end_at, :datetime
+  end
+end
